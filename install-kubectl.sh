cc8f48d60f9ad63575567f36c20c57071f7af7c6
Mihai <danfmihai@hotmail.com>
2020-06-01 14:49:58 -0400

    Installs kubectl, terraform and kops

--- install-kubectl.sh
+++ install-kubectl.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-# Install kubernetes and kops
+# Install kubernetes ,terraform and kops
 
 KUBECTL_BIN=kubectl
- 
+TERRAFORM_ZIP_FILE=terraform_0.12.26_linux_amd64.zip
+TERRAFORM=https://releases.hashicorp.com/terraform/0.12.26/
+TERRAFORM_BIN=terraform
+
+
 install_kubectl () {
     if [ -z $(which $KUBECTL_BIN) ]
        then
@@ -25,6 +29,20 @@ install_kops () {
        fi
 }
 
+ 
+ install_terraform () {
+    if [ -z $(which $TERRAFORM_BIN) ]
+       then
+           wget ${TERRAFORM}/${TERRAFORM_ZIP_FILE}
+           unzip ${TERRAFORM_ZIP_FILE}
+           sudo mv ${TERRAFORM_BIN} /usr/local/bin/${TERRAFORM_BIN}
+           rm -rf ${TERRAFORM_ZIP_FILE}
+    else
+       echo "Terraform is most likely installed"
+    fi
+ 
+}
 
 install_kubectl
 install_kops
+install_terraform
