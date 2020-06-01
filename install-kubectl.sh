273468fb458e8f76df7fe142584f6a90efea3777
Mihai <danfmihai@hotmail.com>
2020-06-01 14:24:57 -0400

    Script ready

---
+++ install-kubectl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+# Install kubernetes and kops
+
+KUBECTL_BIN=kubectl
+ 
+install_kubectl () {
+    if [ -z $(which $KUBECTL_BIN) ]
+       then
+           curl -LO https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/$KUBECTL_BIN
+           chmod +x ${KUBECTL_BIN}
+           sudo mv ${KUBECTL_BIN} /usr/local/bin/${KUBECTL_BIN}
+    else
+       echo "Kubectl is most likely installed"
+    fi
+}
+
+install_kops () {
+    if [ -z $(which kops) ]
+       then
+           curl -LO https://github.com/kubernetes/kops/releases/download/$(curl -s https://api.github.com/repos/kubernetes/kops/releases/latest | grep tag_name | cut -d '"' -f 4)/kops-linux-amd64
+           chmod +x kops-linux-amd64
+           mv kops-linux-amd64 /usr/local/bin/kops
+       else
+           echo "kops is most likely installed"
+       fi
+}
+
+
+install_kubectl
+install_kops
